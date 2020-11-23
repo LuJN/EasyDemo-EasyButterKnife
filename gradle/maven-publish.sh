ef9afcf7b6c655f2251ac055e80342e1d3a37ced
lujunnan <lujunnan@mockuai.com>
2020-11-23 20:37:57 +0800

    Nexus私服搭建、上传

---
+++ gradle/maven-publish.sh
@@ -0,0 +1,4 @@
+./gradlew :butterknife-annotations:clean :butterknife-annotations:uploadArchives
+./gradlew :butterknife-runtime:clean :butterknife-runtime:uploadArchives
+./gradlew :butterknife:clean :butterknife:uploadArchives
+./gradlew :butterknife-compiler:clean :butterknife-compiler:uploadArchives
