7cbb24b4a22f40b16b3debb59447930493534231
lujunnan <lujunnan@mockuai.com>
2020-11-24 19:25:36 +0800

    存在问题的commit

--- gradle/maven-publish.sh
+++ gradle/maven-publish.sh
@@ -1,5 +1,5 @@
 # uploadArchives默认按Release渠道
 ./gradlew :butterknife-annotations:clean :butterknife-annotations:uploadArchives
 ./gradlew :butterknife-runtime:clean :butterknife-runtime:uploadArchives
-./gradlew :butterknife:clean :butterknife:uploadArchives
+./gradlew :butterknife:clean :butterknife:assembleRemoteRelease :butterknife:uploadArchives
 ./gradlew :butterknife-compiler:clean :butterknife-compiler:uploadArchives
